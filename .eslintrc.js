917d285d75488b22cd64248c75b7bc4c906495df
Demetrios Loizides <loizidesdemetris@gmail.com>
2026-01-27 16:11:47 +0200

    fix: add ESLint configuration file

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  env: {
+    node: true,
+    es2020: true,
+  },
+  rules: {
+    // TypeScript strict rules
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/explicit-function-return-type': 'error',
+    '@typescript-eslint/no-non-null-assertion': 'error',
+
+    // Code quality
+    'no-console': 'error',
+    'eqeqeq': ['error', 'always'],
+    'curly': ['error', 'all'],
+  },
+  ignorePatterns: ['dist/', 'node_modules/', '*.js', '**/*.test.ts'],
+};
